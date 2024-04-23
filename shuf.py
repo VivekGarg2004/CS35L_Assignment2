9626259d137f5550563a410f4d88e09e10262e5a
VivekGarg2004 <vivekgarg2004@gmail.com>
2024-04-22 23:23:19 -0700

    Modified shuf.py and added randline.py

--- shuf.py
+++ shuf.py
@@ -1,6 +1,14 @@
 import random, sys
 import argparse
 
+
+def input_range(arg):
+    try:
+        lo, hi = map(int, arg.split('-'))
+        return lo, hi
+    except ValueError:
+        raise argparse.ArgumentTypeError("Invalid input range format. Use LO-HI.")
+    
 parser = argparse.ArgumentParser("""Usage: shuf [OPTION]... [FILE]
                                  or:  shuf -e [OPTION]... [ARG]...
                                  or:  shuf -i LO-HI [OPTION]...
@@ -22,10 +30,22 @@ parser = argparse.ArgumentParser("""Usage: shuf [OPTION]... [FILE]
 
 parser.add_argument("ARGS", nargs="*")
 parser.add_argument("-e", "--echo", action="store_true")
-parser.add_argument("-i", "--input-range", type=str)
+parser.add_argument("-i", "--input-range", type=input_range)
 parser.add_argument('-n', '--head-count', type=int, default=-1)
 parser.add_argument("-r", "--repeat", action="store_true")
 
 
-args = parser.parse_args()
+args = vars(parser.parse_args())
+
+args_option = args.get("ARGS")
+echo_option = args.get("echo")
+repeat_option = args.get("repeat")
+input_option = args.get("input-range")
+head_count_option = args.get("head-count")
+
+
+
+
+
+
 
